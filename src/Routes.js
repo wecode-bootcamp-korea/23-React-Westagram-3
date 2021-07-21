1398466aed588705a26bf172e37739020fc79b41
croissant93 <croissant9393@gmail.com>
2021-07-21 16:05:37 +0900

    Add: Project  초기세팅 완료

---
+++ src/Routes.js
@@ -0,0 +1,9 @@
+
+import LoginJoon from '.';
+
+
+// import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
+<Route exact path='/login-joonsik' component={LoginJoon} />
+<Route exact path='/main-joonsik' component={MainJoon} />
+<Route exact path='/login-jongtaek' component={LoginJongTaek} />
+<Route exact path='/main-jongtaek' component={MainJongTaek} />
